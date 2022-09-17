19159a83b98b0e3b87845474c9012fd3d993a340
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-09-17 12:11:18 -0400

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import './index.css'
-import { FilePicker } from './components/FilePicker'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { FilePicker } from "./components/FilePicker";
 
 function App() {
   return (
-    <div className="App">
-      <FilePicker />
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+        <Route path="/view"></Route>
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FilePicker } from "../components/FilePicker";
+
+const Home = () => {
+  return <FilePicker></FilePicker>;
+};
+
+export default Home;
