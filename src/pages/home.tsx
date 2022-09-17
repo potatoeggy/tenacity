86f1f02d5f7f44957c1fe3307fb00f5a77028bfe
eggy <eggyrules@gmail.com>
2022-09-17 17:35:29 -0400

    fix errors

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { FilePicker } from "../components/FilePicker";
-<<<<<<< HEAD
 import LoadingScreen from "../components/Loading";
-=======
-import './pages.css';
-import ParticlesBg from 'particles-bg'
-
->>>>>>> e09ec4a (home page)
+import "./pages.css";
+import ParticlesBg from "particles-bg";
 
 const Home = () => {
-
-  return <>
-    <ParticlesBg type="circle" bg={true} />
-    <div className='container1'>
-      <FilePicker></FilePicker>
-    </div>
-    
-  </>
-  
+  return (
+    <>
+      <ParticlesBg type="circle" bg={true} />
+      <div className="container1">
+        <FilePicker></FilePicker>
+      </div>
+    </>
+  );
 };
 
 export default Home;
