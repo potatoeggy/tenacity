ebbc2234de4e7df0d16730872a79c6d844d9df4f
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-09-17 15:41:19 -0400

    statistics to view

--- src/pages/view.tsx
+++ src/pages/view.tsx
@@ -3,12 +3,27 @@ import useAssemblyData from "../hooks/useAssemblyData";
 
 const View = () => {
   const [id, setId] = React.useState<string>("");
+  const [wordMap, setWordMap] = React.useState<Record<string, number>>({});
+  const [confidenceLevel, setConfidenceLevel] = React.useState(0);
   const { loading, data, error } = useAssemblyData(id);
   React.useEffect(() => {
     const params = new URLSearchParams(document.location.search);
     const paramId = params.get("id") as string;
     setId(paramId);
   }, []);
+  React.useEffect(() => {
+    if (!data) {
+      return;
+    }
+    let confidenceLevel = 0;
+    const tempWordMap: Record<string, number> = {};
+    for (const word of data.words) {
+      confidenceLevel += word.confidence;
+      tempWordMap[word.text] = tempWordMap[word.text] + 1 || 1;
+    }
+    setWordMap(tempWordMap);
+    setConfidenceLevel(confidenceLevel / data.words.length);
+  }, [data]);
 
   return (
     <div>
