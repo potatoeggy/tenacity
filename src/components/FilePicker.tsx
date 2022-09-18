4503f5d337e9f77ddf2984e9afcb91cf794b85e5
eggy <eggyrules@gmail.com>
2022-09-17 20:07:47 -0400

    fix home styling

--- src/components/FilePicker.tsx
+++ src/components/FilePicker.tsx
@@ -28,7 +28,11 @@ export function FilePicker() {
     ".3ga, .8svx, .aac, .ac3, .aif, .aiff, .alac, .amr, .ape, .au, .dss, .flac, .flv, .m4a, .m4b, .m4p, .m4r, .mp3, .mpga, .ogg, .oga, .mogg, .opus, .qcp, .tta, .voc, .wav, .wma, .wv, .webm, .MTS, .M2TS, .TS, .mov, .mp2, .mp4, .m4p, .m4v, .mxf";
 
   return (
-    <form className="flex flex-col items-start" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col items-start"
+      onSubmit={handleSubmit}
+      style={{ gap: "1rem", display: "flex", flexDirection: "column" }}
+    >
       <div className="header1">Select an audio or video file!</div>
       <label
         className="container2"
@@ -45,7 +49,9 @@ export function FilePicker() {
         <div className="header2">{file?.name ?? "No file selected."}</div>
         <div className="button2">Choose File</div>
       </label>
-      <div className="flex w-full justify-end">
+      <div
+        style={{ display: "flex", justifyContent: "flex-end", width: "100%" }}
+      >
         <button type="submit" className="button1" disabled={!file || submitted}>
           {submitted ? "Loading" : "Upload"}
         </button>
